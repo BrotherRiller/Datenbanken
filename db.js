db097209b057ad592e094bd602bb0e2e6ce13f95
unknown <rimueller@stud.hs-heilbronn.de>
2024-12-08 13:14:31 +0100

    added docker and changes, but still with issues

--- db.js
+++ db.js
@@ -2,10 +2,10 @@ const { Pool } = require('pg');
 
 
 const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'ridesharing',
-  password: 'Popillol123.',
+  user: process.env.POSTGRES_USER || 'postgres',
+  host: process.env.POSTGRES_HOST || 'localhost',
+  database: process.env.POSTGRES_DB || 'ridesharing',
+  password: process.env.POSTGRES_PASSWORD || 'Popillol123.',
   port: 5432,
 });
 
